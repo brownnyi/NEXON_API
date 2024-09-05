546f5243e6dd3b295a35d4929ce908a4c1d5b9b5
brownnyi <brown970523@naver.com>
2024-09-05 12:36:19 +0900

    Update json_to_csv-for_DB.py

--- data/json_to_csv-for_DB.py
+++ data/json_to_csv-for_DB.py
@@ -3,6 +3,7 @@ import json
 import csv
 import pandas as pd
 
+# 예시를 들기 위해 1페이지의 json파일을 가져왔지만 API_KEY를 활용하여 원하는 정보를 모두 불러와 사용 가능
 with open('유니온랭킹1페이지.json','r') as file:
     data = json.load(file)
 
